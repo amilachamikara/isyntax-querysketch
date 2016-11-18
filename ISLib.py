57221862b333126d6b284c2aeb5496b3d3e8335d
amilachamikara <amilachamikara.net@gmail.com>
2016-11-18 14:31:33 +0530

    initial commit

---
+++ ISLib.py
@@ -0,0 +1,18 @@
+import cv2
+
+def optimalSize(img, sqr=800):
+    height, width = img.shape
+
+    ratio = height/width
+
+    if(ratio<=1):
+        h = ratio * sqr
+        w = sqr
+    else:
+        h = sqr
+        w = (1/ratio) * sqr
+
+    scaledRatio = h/height
+
+    output = cv2.resize(img, None, fx=w/width, fy=h/height, interpolation = cv2.INTER_CUBIC)
+    return scaledRatio, output
